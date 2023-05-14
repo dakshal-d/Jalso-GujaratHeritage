78cc35b3ad0c460a66d67bcee45583f7242486cf
Dakshal Dalsania <dakshaldalsania59@gmail.com>
2023-05-14 14:33:36 +0530

    Updated City

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,25 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ContactUs from "./components/ContactUs";
+import Food from "./components/Food";
+import Travel from "./components/Travel";
+import KnowWeather from "./components/KnowWeather";
+import TourismAttraction from "./components/City/TourismAttraction";
+import Surat from "./components/City/Surat";
+import GandhiNagar from "./components/City/GandhiNagar";
+import Jamnagar from "./components/City/Jamnagar";
+import Porbandar from "./components/City/Porbandar"; 
+import Vadodra from "./components/City/Vadodra";
+import Junagadh from "./components/City/Junagadh";
+import Ahmedabad from "./components/City/Ahmedabad";
+import Rajkot from "./components/City/Rajkot"
 
 import {
   BrowserRouter as Router,
   Routes,
   Route
 } from "react-router-dom";
-import Food from "./components/Food";
-import Travel from "./components/Travel";
-import KnowWeather from "./components/KnowWeather";
-import TourismAttraction from "./components/City/TourismAttraction";
-import Surat from "./components/City/Surat";
+
 function App() {
   return (
     <div className="">
@@ -27,8 +35,15 @@ function App() {
           <Route exact path="/travel/KnowWeather" element={ <KnowWeather/>} />
           <Route exact path="/travel/TourismAttraction" element={ <TourismAttraction/>} />
           <Route exact path="/travel/Surat" element={ <Surat/>} />
+          <Route exact path="/travel/GandhiNagar" element={ <GandhiNagar/>} />
+          <Route exact path="/travel/Jamnagar" element={ <Jamnagar/>} />
           <Route exact path="/about" element={ <About />} />
           <Route exact path="/ContactUs" element={ <ContactUs />} />
+          <Route exact path="/travel/Porbandar" element={ <Porbandar/>} />
+          <Route exact path="/travel/Vadodra" element={ <Vadodra/>} />
+          <Route exact path="/travel/Junagadh" element={ <Junagadh/>} />
+          <Route exact path="/travel/Ahmedabad" element={ <Ahmedabad/>} />
+          <Route exact path="/travel/Rajkot" element={ <Rajkot/>} />
         </Routes>
         <Footer/>
       </Router>
---
+++ src/components/City/Ahmedabad.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Ahmedabad() {
+  return (
+    <div>Ahmedabad</div>
+  )
+}
+
+export default Ahmedabad
